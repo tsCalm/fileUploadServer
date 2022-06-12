1eb6777650a05159cf366c719b18f08af2b44dba
tsCalm <dlatkdals93@gmail.com>
2022-06-12 23:14:07 +0900

    update: file module, config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfigAsync } from './config/typeorm.config';
+import { ConfigModule } from '@nestjs/config';
+import * as path from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+      expandVariables: true,
+      envFilePath:
+        process.env.NODE_ENV === 'dev'
+          ? path.join(__dirname, '../**/*.entity.ts')
+          : path.join(__dirname, '../**/*.entity.js'),
+    }),
+    TypeOrmModule.forRootAsync(typeOrmConfigAsync),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
